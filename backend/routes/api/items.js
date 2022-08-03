9ed43e6b506dacb2784b70617a914a2a0db10157
Purushottam khedre <purushottamkhedre123@gmail.com>
2022-08-04 00:19:55 +0530

    implement title query

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -40,6 +40,7 @@ router.get("/", auth.optional, function(req, res, next) {
   var query = {};
   var limit = 100;
   var offset = 0;
+  var title='';
 
   if (typeof req.query.limit !== "undefined") {
     limit = req.query.limit;
@@ -51,6 +52,9 @@ router.get("/", auth.optional, function(req, res, next) {
 
   if (typeof req.query.tag !== "undefined") {
     query.tagList = { $in: [req.query.tag] };
+  }
+  if (typeof req.query.title !== "undefined") {
+    title = req.query.title;
   }
 
   Promise.all([
