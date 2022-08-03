1b4f568353095a741722f3087cf22fac70c78cda
Purushottam khedre <purushottamkhedre123@gmail.com>
2022-08-04 04:33:45 +0530

    formatting all files

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -3,25 +3,25 @@ import logo from "../../imgs/logo.png";
 import agent from "../../agent";
 
 const Banner = (props) => {
-  let [searchTerm, setSearchTerm] = React.useState("");
+    let [searchTerm, setSearchTerm] = React.useState("");
 
-  if (searchTerm.length >= 3)
-    props.onSearch(searchTerm, agent.Items.bySearch(searchTerm));
+    if (searchTerm.length >= 3)
+        props.onSearch(searchTerm, agent.Items.bySearch(searchTerm));
 
-  return (
-    <div className="banner text-white">
-      <div className="container p-4 text-center">
-        <img src={logo} alt="banner" />
-        <div>
-          <span id="get-part">A place to get</span>
-          <span>
-            <input type="text" id="search-box" className="m-1 p-1 pl-2" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="search" />
-          </span>
-          <span> the cool stuff.</span>
+    return (
+        <div className="banner text-white">
+            <div className="container p-4 text-center">
+                <img src={logo} alt="banner" />
+                <div>
+                    <span id="get-part">A place to get</span>
+                    <span>
+                        <input type="text" id="search-box" className="m-1 p-1 pl-2" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="search" />
+                    </span>
+                    <span> the cool stuff.</span>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Banner;
--- frontend/src/reducers/itemList.js
+++ frontend/src/reducers/itemList.js
@@ -15,82 +15,82 @@ import {
 
 const reducer = (state = {}, action) => {
   switch (action.type) {
-    case ITEM_FAVORITED:
-    case ITEM_UNFAVORITED:
-      return {
-        ...state,
-        items: state.items.map((item) => {
-          if (item.slug === action.payload.item.slug) {
-            return {
-              ...item,
-              favorited: action.payload.item.favorited,
-              favoritesCount: action.payload.item.favoritesCount,
-            };
-          }
-          return item;
-        }),
-      };
-    case SET_PAGE:
-      return {
-        ...state,
-        items: action.payload.items,
-        itemsCount: action.payload.itemsCount,
-        currentPage: action.page,
-      };
-    case APPLY_TAG_FILTER:
-      return {
-        ...state,
-        pager: action.pager,
-        items: action.payload.items,
-        itemsCount: action.payload.itemsCount,
-        tab: null,
-        tag: action.tag,
-        currentPage: 0,
-      };
-    case APPLY_SEARCH_FILTER:
-      return {
-        ...state,
-        items: action.payload.items,
-        itemsCount: action.payload.itemsCount,
-        tab: null,
-        currentPage: 0,
-      };
-    case HOME_PAGE_LOADED:
-      return {
-        ...state,
-        pager: action.pager,
-        tags: action.payload[0].tags,
-        items: action.payload[1].items,
-        itemsCount: action.payload[1].itemsCount,
-        currentPage: 0,
-        tab: action.tab,
-      };
-    case HOME_PAGE_UNLOADED:
-      return {};
-    case CHANGE_TAB:
-      return {
-        ...state,
-        pager: action.pager,
-        items: action.payload.items,
-        itemsCount: action.payload.itemsCount,
-        tab: action.tab,
-        currentPage: 0,
-        tag: null,
-      };
-    case PROFILE_PAGE_LOADED:
-    case PROFILE_FAVORITES_PAGE_LOADED:
-      return {
-        ...state,
-        pager: action.pager,
-        items: action.payload[1].items,
-        itemsCount: action.payload[1].itemsCount,
-        currentPage: 0,
-      };
-    case PROFILE_PAGE_UNLOADED:
-    case PROFILE_FAVORITES_PAGE_UNLOADED:
-      return {};
-    default:
-      return state;
+      case ITEM_FAVORITED:
+      case ITEM_UNFAVORITED:
+          return {
+              ...state,
+              items: state.items.map((item) => {
+                  if (item.slug === action.payload.item.slug) {
+                      return {
+                          ...item,
+                          favorited: action.payload.item.favorited,
+                          favoritesCount: action.payload.item.favoritesCount,
+                      };
+                  }
+                  return item;
+              }),
+          };
+      case SET_PAGE:
+          return {
+              ...state,
+              items: action.payload.items,
+              itemsCount: action.payload.itemsCount,
+              currentPage: action.page,
+          };
+      case APPLY_TAG_FILTER:
+          return {
+              ...state,
+              pager: action.pager,
+              items: action.payload.items,
+              itemsCount: action.payload.itemsCount,
+              tab: null,
+              tag: action.tag,
+              currentPage: 0,
+          };
+      case APPLY_SEARCH_FILTER:
+          return {
+              ...state,
+              items: action.payload.items,
+              itemsCount: action.payload.itemsCount,
+              tab: null,
+              currentPage: 0,
+          };
+      case HOME_PAGE_LOADED:
+          return {
+              ...state,
+              pager: action.pager,
+              tags: action.payload[0].tags,
+              items: action.payload[1].items,
+              itemsCount: action.payload[1].itemsCount,
+              currentPage: 0,
+              tab: action.tab,
+          };
+      case HOME_PAGE_UNLOADED:
+          return {};
+      case CHANGE_TAB:
+          return {
+              ...state,
+              pager: action.pager,
+              items: action.payload.items,
+              itemsCount: action.payload.itemsCount,
+              tab: action.tab,
+              currentPage: 0,
+              tag: null,
+          };
+      case PROFILE_PAGE_LOADED:
+      case PROFILE_FAVORITES_PAGE_LOADED:
+          return {
+              ...state,
+              pager: action.pager,
+              items: action.payload[1].items,
+              itemsCount: action.payload[1].itemsCount,
+              currentPage: 0,
+          };
+      case PROFILE_PAGE_UNLOADED:
+      case PROFILE_FAVORITES_PAGE_UNLOADED:
+          return {};
+      default:
+          return state;
   }
 };
 
