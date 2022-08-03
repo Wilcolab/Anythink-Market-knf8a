c3601539181fba41258f60da710dbc7f93ed6af5
Purushottam khedre <purushottamkhedre123@gmail.com>
2022-08-04 05:10:54 +0530

    implement search mechenism on frontend UI

--- frontend/src/reducers/itemList.js
+++ frontend/src/reducers/itemList.js
@@ -3,6 +3,7 @@ import {
   ITEM_UNFAVORITED,
   SET_PAGE,
   APPLY_TAG_FILTER,
+  APPLY_SEARCH_FILTER,
   HOME_PAGE_LOADED,
   HOME_PAGE_UNLOADED,
   CHANGE_TAB,
@@ -44,6 +45,14 @@ const reducer = (state = {}, action) => {
         itemsCount: action.payload.itemsCount,
         tab: null,
         tag: action.tag,
+        currentPage: 0,
+      };
+    case APPLY_SEARCH_FILTER:
+      return {
+        ...state,
+        items: action.payload.items,
+        itemsCount: action.payload.itemsCount,
+        tab: null,
         currentPage: 0,
       };
     case HOME_PAGE_LOADED:
