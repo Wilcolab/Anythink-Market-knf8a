37eaff198e9d069d2062fe7838212cde4b17c8e8
Purushottam khedre <purushottamkhedre123@gmail.com>
2022-08-04 03:54:30 +0530

    set mechanism of search in UI

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -56,6 +56,10 @@ const Items = {
     requests.get(`/items?seller=${encode(seller)}&${limit(500, page)}`),
   byTag: (tag, page) =>
     requests.get(`/items?tag=${encode(tag)}&${limit(1000, page)}`),
+  bySearch: (searchTerm) =>{
+    console.log("puru",searchTerm);
+    return requests.get(`/items?title=${searchTerm}`)
+  },
   del: (slug) => requests.del(`/items/${slug}`),
   favorite: (slug) => requests.post(`/items/${slug}/favorite`),
   favoritedBy: (seller, page) =>
