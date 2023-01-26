eb9eb3c00c060f46dc890967d9f810312cba427c
WilcoApp <hello@wilco.gg>
2023-01-26 03:10:38 +0000

    Adding DeleteButton.js

--- frontend/src/components/Item/DeleteButton.js
+++ frontend/src/components/Item/DeleteButton.js
@@ -5,7 +5,10 @@ import { DELETE_COMMENT } from "../../constants/actionTypes";
 
 const mapDispatchToProps = (dispatch) => ({
   onClick: (payload, commentId) =>
-    dispatch({ type: DELETE_COMMENT, payload, commentId }),
+    dispatch({ 
+      type: DELETE_COMMENT, 
+      payload, commentId 
+    }),
 });
 
 const DeleteButton = (props) => {
